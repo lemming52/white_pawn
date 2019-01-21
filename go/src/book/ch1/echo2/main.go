4753ff2a55d005d19d022bba261b53afe58b43ce
Andrew Strange <andrew@cytora.com>
2019-01-21 20:46:26 +0000

    Add CH1 exercises

---
+++ go/src/book/ch1/echo2/main.go
@@ -0,0 +1,23 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 6.
+//!+
+
+// Echo2 prints its command-line arguments.
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	s, sep := "", " "
+	for i, arg := range os.Args {
+		s = sep + arg
+		fmt.Println(i, s)
+	}
+}
+
+//!-
---
+++ go/src/book/ch1/lissajous/main.go
@@ -0,0 +1,92 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// Run with "web" command-line argument for web server.
+// See page 13.
+//!+main
+
+// Lissajous generates GIF animations of random Lissajous figures.
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"math"
+	"math/rand"
+	"os"
+	"strconv"
+)
+
+//!-main
+// Packages not needed by version in book.
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+//!+main
+
+var palette = []color.Color{color.Black, color.RGBA{0xff, 0x11, 0x11, 0xff}, color.RGBA{0x11, 0x11, 0xff, 0xff}, color.RGBA{0x11, 0xff, 0x11, 0xff}}
+
+func main() {
+	//!-main
+	// The sequence of images is deterministic unless we seed
+	// the pseudo-random number generator using the current time.
+	// Thanks to Randall McPherson for pointing out the omission.
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	if len(os.Args) > 1 && os.Args[1] == "web" {
+		//!+http
+		handler := func(w http.ResponseWriter, r *http.Request) {
+			if err := r.ParseForm(); err != nil {
+				log.Print(err)
+			}
+			cycles := 5
+			for k, v := range r.Form {
+				if k == "cycles" {
+					cycles, _ = strconv.Atoi(v[0])
+				}
+			}
+			lissajous(w, cycles)
+		}
+		http.HandleFunc("/", handler)
+		//!-http
+		log.Fatal(http.ListenAndServe("localhost:8000", nil))
+		return
+	}
+	//!+main
+	lissajous(os.Stdout, 5)
+}
+
+func lissajous(out io.Writer, cycles int) {
+	const (
+		res     = 0.001 // angular resolution
+		size    = 100   // image canvas covers [-size..+size]
+		nframes = 64    // number of animation frames
+		delay   = 8     // delay between frames in 10ms units
+	)
+	freq := rand.Float64() * 3.0 // relative frequency of y oscillator
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0 // phase difference
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		var counter uint8
+		for t := 0.0; t < 2*math.Pi*float64(cycles); t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			counter++
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5),
+				counter%3)
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(out, &anim) // NOTE: ignoring encoding errors
+}
+
+//!-main
