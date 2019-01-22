12938afbff8f08cc974f56e868b37ef976b1c21e
Andrew Strange <andrew@cytora.com>
2019-01-22 18:52:36 +0000

    Add ex 3.10
    Add a non recursive buffered comma

---
+++ go/src/book/ch3/comma/main.go
@@ -0,0 +1,54 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 73.
+
+// Comma prints its argument numbers with a comma at each power of 1000.
+//
+// Example:
+// 	$ go build gopl.io/ch3/comma
+//	$ ./comma 1 12 123 1234 1234567890
+// 	1
+// 	12
+// 	123
+// 	1,234
+// 	1,234,567,890
+//
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+)
+
+func main() {
+	for i := 1; i < len(os.Args); i++ {
+		fmt.Printf("  %s\n", comma(os.Args[i]))
+		fmt.Printf("  %s\n", bufferedComma(os.Args[i]))
+	}
+}
+
+//!+
+// comma inserts commas in a non-negative decimal integer string.
+func comma(s string) string {
+	n := len(s)
+	if n <= 3 {
+		return s
+	}
+	return comma(s[:n-3]) + "," + s[n-3:]
+}
+
+func bufferedComma(s string) string {
+	var buf bytes.Buffer
+	length := len(s)
+	for i := 0; i < length; i++ {
+		if (length-i)%3 == 0 {
+			buf.WriteByte(',')
+		}
+		buf.WriteByte(s[i])
+	}
+	return buf.String()
+}
+
+//!-
