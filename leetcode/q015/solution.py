24027a895b9de07ba5828c2d7bb132242e7e68e8
Andrew Strange <andrew@gumboot.co.uk>
2020-03-31 17:20:09 +0100

    Add a much more elegant solution

--- leetcode/q015/solution.py
+++ leetcode/q015/solution.py
@@ -9,56 +9,29 @@ The solution set must not contain duplicate triplets.
 from typing import Dict, List
 
 class Solution:
+
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        neg, zero, pos = [], [], []
-        results = []
-
-        for n in nums:
-            if n < 0:
-                neg.append(n)
-            elif n > 0:
-                pos.append(n)
-            else:
-                zero.append(0)
-
-        n_zero = len(zero)
-        if n_zero > 0:
-            if n_zero >= 3:
-                results.append([0,0,0])
-            check_with_zero(neg, pos, results)
+        nums = sorted(nums)
         found = {}
-        for i, a in enumerate(neg):
-            check_two_pos(pos, a, results, found)
-            check_one_pos(neg[i+1:], pos, a, results, found)
+        results = []
+        for i, a in enumerate(nums):
+            j = i + 1
+            k = len(nums) - 1
+            if a > 0:
+                break
+            while j < k:
+                b = nums[j]
+                c = nums[k]
+                total = a + b + c
+                if total == 0:
+                    key = f"{a}{b}{c}"
+                    if not key in found:
+                        found[key] = True
+                        results.append([a, b, c])
+                    j += 1
+                    k -= 1
+                elif total > 0:
+                    k -= 1
+                else:
+                    j += 1
         return results
-
-def check_with_zero(neg, pos: List[int], res: List[List[int]]) -> None:
-    found = {}
-    for a in neg:
-        for b in pos:
-            if a + b == 0:
-                key = f'{a}{b}'
-                if not key in found:
-                    res.append([a, 0, b])
-                    found[key] = True
-                break
-
-def check_two_pos(pos: List[int], a: int, res: List[List[int]], found: Dict[str, bool]) -> None:
-    for i, b in enumerate(pos):
-        for c in pos[i+1:]:
-            if (b + c + a) == 0:
-                key = "".join([str(x) for x in sorted([a, b, c])])
-                if not key in found:
-                    res.append([a, b, c])
-                    found[key] = True
-                break
-
-def check_one_pos(neg, pos: List[int], a: int, res: List[List[int]], found: Dict[str, bool]) -> None:
-    for b in neg:
-        for c in pos:
-            if (b + c + a) == 0:
-                key = "".join([str(x) for x in sorted([a, b, c])])
-                if not key in found:
-                    res.append([a, b, c])
-                    found[key] = True
-                break
