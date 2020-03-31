2ddcbc803dcab4896749b22dec9192d156f3d735
Andrew Strange <andrew@gumboot.co.uk>
2020-03-31 14:42:37 +0100

    Tidy up

--- leetcode/q015/solution.py
+++ leetcode/q015/solution.py
@@ -12,21 +12,16 @@ class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         results = []
         length = len(nums)
-        found = {}, {}
+        found = {}
         for i, a in enumerate(nums[:-2]):
             for j in range(i+1, length-1):
                 b = nums[j]
                 for k in range(j+1, length):
                     c = nums[k]
-                    print(a, i, b, j, c, k)
                     if (a + b + c) == 0:
                         res = [a, b, c]
-                        key = "".join(sorted(res))
+                        key = "".join([str(x) for x in sorted(res)])
                         if not key in found:
                             results.append(res)
                             found[key] = True
         return results
-
-
-def alreadyFound(a, b, c: int, products, sums: Dict[int, bool]):
-    return (a * b * c in products and a + b + c in sums)
