53858a00606b8d5bd0bb47bced32ecb7a35b691d
Andrew Strange <andrew@gumboot.co.uk>
2020-03-31 13:11:09 +0100

    Add brute force solution
    Add a brute force solution that computes the value for each and
    every possible pair.

---
+++ leetcode/q011/solution.py
@@ -0,0 +1,22 @@
+"""
+Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai).
+n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0). Find two lines, which together with x-axis forms a container,
+such that the container contains the most water.
+
+Note: You may not slant the container and n is at least 2.
+"""
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        entries = len(height)
+        res = 0
+        for i in range(entries):
+            for j in range(i, entries):
+                area = getArea(i, height[i], j, height[j])
+                if area > res:
+                    res = area
+        return res
+
+def getArea(x1, y1, x2, y2) -> int:
+    height = y1 if y1 < y2 else y2
+    return (x2 - x1) * height
