abf8e4851b99b15ad0fa022e1452f4ed5d28fbb1
Andrew Strange <andrew@gumboot.co.uk>
2020-04-05 14:01:13 +0100

    Add massively overcomplicated solution

---
+++ hackerrank/leftrotation/solution.py
@@ -0,0 +1,87 @@
+"""
+A left rotation operation on an array shifts each of the array's elements  unit to the left. For example, if  left rotations are performed on array , then the array would become .
+
+Given an array  of  integers and a number, , perform  left rotations on the array. Return the updated array to be printed as a single line of space-separated integers.
+
+Function Description
+
+Complete the function rotLeft in the editor below. It should return the resulting array of integers.
+
+rotLeft has the following parameter(s):
+
+An array of integers .
+An integer , the number of rotations.
+Input Format
+
+The first line contains two space-separated integers  and , the size of  and the number of left rotations you must perform.
+The second line contains  space-separated integers .
+
+Constraints
+
+Output Format
+
+Print a single line of  space-separated integers denoting the final state of the array after performing  left rotations.
+
+Sample Input
+
+5 4
+1 2 3 4 5
+Sample Output
+
+5 1 2 3 4
+Explanation
+
+When we perform  left rotations, the array undergoes the following sequence of changes:
+
+"""
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from typing import List
+
+# Complete the rotLeft function below.
+def rotLeft(a: List[int], d: int) -> List[int]:
+    length = len(a)
+    if length < 2:
+        return a
+    if d > length:
+        d = d - length
+    if d > length // 2:
+        for i in range(0, length - d):
+            rotRightSingle(a, length)
+    else:
+        for i in range(0, d):
+            rotLeftSingle(a, length-1)
+    return a
+
+def rotLeftSingle(a: List[int], length: int) -> None:
+    for i in range(0, length):
+        a[i], a[i+1] = a[i+1], a[i]
+
+def rotRightSingle(a: List[int], length: int) -> None:
+    for i in range(length - 1, 0, -1):
+        a[i], a[i-1] = a[i-1], a[i]
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nd = input().split()
+
+    n = int(nd[0])
+
+    d = int(nd[1])
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = rotLeft(a, d)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
